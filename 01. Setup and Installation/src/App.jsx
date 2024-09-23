8d649ef95d52d02bf612b0fac4b0850abd754b2a
Ankit Gupta <ankit05gupta01@gmail.com>
2024-09-24 01:26:42 +0530

    Setup and Installation

---
+++ 01. Setup and Installation/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>Lorem ipsum dolor sit amet consectetur.</h1>
+      <h3>Lorem ipsum dolor sit.</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi modi nihil sapiente soluta! Sit, enim!</p>
+    </div>
+  )
+}
+
+export default App
